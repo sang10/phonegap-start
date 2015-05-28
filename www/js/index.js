db6c4ae90678b2a0b04300485cd664d0b8513702
sang10 <sangeeta.devi@niit.com>
2015-05-28 08:35:17 +0530

    Revert "Rectangle area app"
    This reverts commit 35de64fde6a1e130e38d6f284b83221546e0985e.

--- www/js/index.js
+++ www/js/index.js
@@ -27,7 +27,6 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-document.getElementById('btnCalculate').addEventListener('click', this.onCalculate, false);
     },
     // deviceready Event Handler
     //
@@ -46,19 +45,5 @@ document.getElementById('btnCalculate').addEventListener('click', this.onCalcula
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-onCalculate: function() {
-var b = document.getElementById('leng').value;
-var h = document.getElementById('bred').value;
-if (b.length == 0 || h.length == 0)
-document.getElementById('res').innerHTML = "ERROR: Please fill all the fields.";
-else if (b == 0 || h == 0)
-document.getElementById('res').innerHTML = "ERROR: Please enter valid values.";
-else {
-var x = parseInt(b);
-var y = parseInt(h);
-var area = x * y;
-document.getElementById('res').innerHTML = "Area = " + area;
-}
-}
+    }
 };
